72376fd1b671666218f50399592c31d25e5ff970
Shane Perry <perrysm98@gmail.com>
2023-04-29 15:53:49 -0500

    Added Initial data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Item (id, name, price, description, category) VALUES (1, 'Dog Food', 59.00, 'This is dog food.', 'Dog');
+INSERT INTO Item (id, name, price, description, category) VALUES (2, 'Cat Food', 59.00, 'This is cat food.', 'Cat');
+INSERT INTO Item (id, name, price, description, category) VALUES (3, 'Bird Food', 59.00, 'This is bird food.', 'Bird');
+INSERT INTO Item (id, name, price, description, category) VALUES (4, 'Snail Food', 59.00, 'This is snail food.', 'Snail');
+INSERT INTO Item (id, name, price, description, category) VALUES (5, 'Rabbit Food', 59.00, 'This is rabbit food.', 'Rabbit');

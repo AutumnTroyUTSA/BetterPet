c7ae4dcfccf3cb508607a1018dcd36db2bba5110
Shane Perry <perrysm98@gmail.com>
2023-05-05 19:06:28 -0500

    Added images to some items

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Martingale Collar', 25.00, 'Training collar for dogs.', 'Dog', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Prong Collar', 21.00, 'Training collar for aggressive pulling dogs.', 'Dog', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Standard Dog Leash', 12.00, 'Standard leash for dogs.', 'Dog', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Retractable Leash', 8.00, 'Retractable leash for dogs.', 'Dog', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Slip lead', 2.00, 'Collar/leash in one for dogs.', 'Dog', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cage Muzzle', 10.00, 'Muzzle for dogs.', 'Dog', 'placeholder');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Prong Collar', 21.00, 'Training collar for aggressive pulling dogs.', 'Dog', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=120488714');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Standard Dog Leash', 12.00, 'Standard leash for dogs.', 'Dog', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=57472000');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Retractable Leash', 8.00, 'Retractable leash for dogs.', 'Dog', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=584906597');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Slip lead', 2.00, 'Collar/leash in one for dogs.', 'Dog', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=198573381');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cage Muzzle', 10.00, 'Muzzle for dogs.', 'Dog', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=334018419');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Strap Muzzle', 8.00, 'Muzzle for dogs.', 'Dog', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Dog Harness', 30.00, 'Harness for dogs.', 'Dog', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Working Dog Backpack', 60.00, 'Backpack for working dog breeds.', 'Dog', 'placeholder');
@@ -12,27 +12,33 @@ INSERT INTO Item (name, price, description, category, image_path) VALUES ('Hot W
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Poop Bags', 5.00, 'Bags for picking up dog poop.', 'Dog', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Paw Pad Balm', 12.00, 'Balm for dry/cracked dog paw pads.', 'Dog', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Nose Balm', 12.00, 'Balm for dry/cracked dog noses.', 'Dog', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Poop Bag Holder', 3.00, 'Holder for dog poop bags.', 'Dog', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Nail Trimmer(Dogs)', 8.00, 'Nail Trimmer for dogs.', 'Dog', 'placeholder');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Poop Bag Holder', 3.00, 'Holder for dog poop bags.', 'Dog', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=247630871');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Nail Trimmer(Dogs)', 8.00, 'Nail Trimmer for dogs.', 'Dog', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=46308473');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Dog Food', 60.00, 'Food for dogs', 'Dog', 'placeholder');
+
+
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Quick Release Collar', 3.00, 'Quick Release Collar for cats.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Nail Caps', 2.00, 'Nail caps for cats.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Carrier', 25.00, 'Handheld Kennel for cats.', 'Cat', 'placeholder');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Nail Caps', 2.00, 'Nail caps for cats.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=355947223');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Carrier', 25.00, 'Handheld Kennel for cats.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=105027327');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Collar Charm', 8.00, 'Charm for cat collars.', 'Cat', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Nail Clippers(Cats)', 8.00, 'Cat nail clippers.', 'Cat', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Hair Ball Relief Treats', 12.00, 'Treats to help relieve hair balls.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Litter Box', 10.00, 'Litter Box for cats.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Litter', 25.00, 'Litter for cats.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Nip', 6.00, 'Cat nip.', 'Cat', 'placeholder');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Litter Box', 10.00, 'Litter Box for cats.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=129569200');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Litter', 25.00, 'Litter for cats.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=152373925');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Nip', 6.00, 'Cat nip.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=443743254');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Scratching Post', 40.00, 'Scratching post for cats.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Tree', 60.00, 'Cat tree.', 'Cat', 'placeholder');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Tree', 60.00, 'Cat tree.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=109362719');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Treats', 10.00, 'Treats for cats.', 'Cat', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Slicker Brush', 12.00, 'Brush for cats.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Litter Mat', 12.00, 'Mat for litter boxes.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Mouse Toy', 1.00, 'Mouse toy for cats.', 'Cat', 'placeholder');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Litter Mat', 12.00, 'Mat for litter boxes.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=487011271');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Mouse Toy', 1.00, 'Mouse toy for cats.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=204946584');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Pole Toy', 1.50, 'Pole toy for cats.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Bed', 25.00, 'Bed for cats.', 'Cat', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Food', 20.00, 'Food for cats.', 'Cat', 'placeholder');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Bed', 25.00, 'Bed for cats.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=91597814');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Cat Food', 20.00, 'Food for cats.', 'Cat', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=91625736');
+
+
+
+
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Bird Cage', 35.00, 'Cage for a bird.', 'Small_Animal', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Hamster Cage', 70.00, 'Cage for a hamster.', 'Small_Animal', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Rabbit Cage', 70.00, 'Cage for a rabbit.', 'Small_Animal', 'placeholder');
@@ -44,7 +50,7 @@ INSERT INTO Item (name, price, description, category, image_path) VALUES ('Food 
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Ferret Sling', 12.00, 'Sling/hammock for ferrets.', 'Small_Animal', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Sugar Glider Canopy', 18.00, 'Canopy for Sugar Gliders.', 'Small_Animal', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Small Animal Igloo', 2.00, 'Igloo shaped house for small animals.', 'Small_Animal', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Timothy Hay', 12.00, 'Food for small rodents.', 'Small_Animal', 'placeholder');
-INSERT INTO Item (name, price, description, category, image_path) VALUES ('Alfalfa Hay', 12.00, 'Food for small rodents.', 'Small_Animal', 'placeholder');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Timothy Hay', 12.00, 'Food for small rodents.', 'Small_Animal', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=109661917');
+INSERT INTO Item (name, price, description, category, image_path) VALUES ('Alfalfa Hay', 12.00, 'Food for small rodents.', 'Small_Animal', 'https://stock.adobe.com/Library/urn:aaid:sc:US:41f71f13-343d-47ec-a1ed-a430b65bd619?asset_id=394635362');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Yogurt Drops', 5.00, 'Probiotic treats for small animals.', 'Small_Animal', 'placeholder');
 INSERT INTO Item (name, price, description, category, image_path) VALUES ('Hamster Tunnel', 80.00, 'Tunnel for hamsters.', 'Small_Animal', 'placeholder');

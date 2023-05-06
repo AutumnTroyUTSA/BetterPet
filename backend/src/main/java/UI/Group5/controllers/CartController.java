0d2c0040661f2301ba00b43e7959c91d71c7ea23
Shane Perry <perrysm98@gmail.com>
2023-05-05 19:46:01 -0500

    Fixed Cart Again

--- backend/src/main/java/UI/Group5/controllers/CartController.java
+++ backend/src/main/java/UI/Group5/controllers/CartController.java
@@ -1,10 +1,10 @@
 package UI.Group5.controllers;
 
-import UI.Group5.models.Cart;
 import UI.Group5.models.Item;
 import UI.Group5.repo.CartRepository;
 import UI.Group5.repo.ItemRepository;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Basic;
+import jakarta.persistence.FetchType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,14 +21,26 @@ public class CartController {
     @Autowired
     private ItemRepository itemRepository;
 
-    List<Item> cart = new ArrayList<>();
+    @Basic(fetch = FetchType.LAZY)
+    List<Optional<Item>> cart = new ArrayList<Optional<Item>>();
     /*@GetMapping(value = "/all")
     public List<Cart> getCart() {return cartRepository.findAll();}*/
-    @RequestMapping("/add/{id}")
-    public void newCartItem(@RequestBody Item newCartItem, @PathVariable Long id) {cart.add(newCartItem);}
+    /*@RequestMapping("/add/{id}")
+    public void newCartItem(@RequestBody Item newCartItem, @PathVariable Long id) {cart.add(newCartItem);}*/
 
+    public void setCart(List<Optional<Item>> cart) {
+        this.cart = cart;
+    }
+
+    @GetMapping("/add/{id}")
+    public void/*Optional<Item>*/ getItem(@PathVariable Long id) {
+        Optional<Item> newItem = itemRepository.findById(id);
+        cart.add(newItem);
+        //return newItem;
+    }
     @GetMapping("/showCart")
-    public List<Item> getCart() {return cart;}
+    public @ResponseBody List<Optional<Item>> getCart() {return cart;}
+
 
    /* @PutMapping("/update/{id}")
     public Optional<Cart> updateCart(@RequestBody Cart newCart, @PathVariable Long id) {
